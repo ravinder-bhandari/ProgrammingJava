cb709b8edec26308ce16f564a706482136c38bcd
ravinder_bhandari_grappus <ravinder@grappus.com>
2018-10-04 01:33:17 +0530

    [Setup] basic project setup to run Java Application in Android Studio

---
+++ src/main/java/sample/SampleMain.java
@@ -0,0 +1,14 @@
+package sample;
+
+/**
+ * Created by ravinder on 04/10/18.
+ */
+
+public class SampleMain {
+
+    public static void main(String[] args) {
+
+        String hello = "Hello World";
+        System.out.println(hello);
+    }
+}
